0ca6b4385742507210bd47c57645767eb385675e
Ammad <49301198+ammadibnearshad@users.noreply.github.com>
2020-10-08 20:51:12 +0500

    Add files via upload

---
+++ task1/index.php
@@ -0,0 +1,372 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Paratemporal Night Vision Equipment For Paranormal Investigations</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+<style type="text/css">
+body {
+	background-color: #121212;
+	margin-left: 0px;
+	margin-top: 0px;
+}
+.screen-wr {
+  position: relative;
+  width: 620px;
+  margin: 0 auto;
+  padding: 20px 0 0 0;
+  text-align: center;
+}
+
+.screen-wr A {
+  font-size: 24px;
+  text-decoration: none;
+  color: black;
+  border-bottom: 1px solid transparent;
+  -webkit-transition: border-color 150ms ease;
+  transition: border-color 150ms ease;
+  text-transform: uppercase;
+}
+
+.screen-wr A:hover { border-color: black; }
+
+.tv {
+  width: 100%;
+  margin: 15px 0 0 0;
+  position: relative;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  background: #231f20;
+  -webkit-box-shadow: 13px 13px rgba(0, 0, 0, 0.16);
+  box-shadow: 13px 13px rgba(0, 0, 0, 0.16);
+}
+
+.screen-inner {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  background-color: black;
+  overflow: hidden;
+}
+
+.screen-content {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  -webkit-background-size: cover;
+  background-size: cover;
+  background-position: center center;
+}
+
+.screen-content:after {
+  width: 100%;
+  height: 100%;
+  content: "";
+  position: absolute;
+  background: white;
+  opacity: 0;
+  left: 0;
+  top: 0;
+}
+
+.tv._off .screen-content {
+  -webkit-animation: shutdown 400ms linear;
+  animation: shutdown 400ms linear;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+}
+
+.tv._off .screen-content:after {
+  -webkit-animation: shutdown-opa 400ms linear;
+  animation: shutdown-opa 400ms linear;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+}
+
+ @-webkit-keyframes 
+shutdown {  0% {
+ -webkit-transform: scale3d(1, 1, 1);
+ transform: scale3d(1, 1, 1);
+}
+ 20% {
+ -webkit-transform: scale3d(1, 1.6, 1);
+ transform: scale3d(1, 1.6, 1);
+}
+ 50% {
+ -webkit-transform: scale3d(1, 0.005, 1);
+ transform: scale3d(1, 0.005, 1);
+}
+ 100% {
+ -webkit-transform: scale3d(0, 0, 1);
+ transform: scale3d(0, 0, 1);
+}
+}
+ @keyframes 
+shutdown {  0% {
+ -webkit-transform: scale3d(1, 1, 1);
+ transform: scale3d(1, 1, 1);
+}
+ 20% {
+ -webkit-transform: scale3d(1, 1.6, 1);
+ transform: scale3d(1, 1.6, 1);
+}
+ 50% {
+ -webkit-transform: scale3d(1, 0.005, 1);
+ transform: scale3d(1, 0.005, 1);
+}
+ 100% {
+ -webkit-transform: scale3d(0, 0, 1);
+ transform: scale3d(0, 0, 1);
+}
+}
+@-webkit-keyframes 
+shutdown-opa {  0% {
+ opacity: 0;
+}
+ 50% {
+ opacity: 1;
+}
+ 100% {
+ opacity: 1;
+}
+}
+@keyframes 
+shutdown-opa {  0% {
+ opacity: 0;
+}
+ 50% {
+ opacity: 1;
+}
+ 100% {
+ opacity: 1;
+}
+}
+.main-conatiner{
+  background:#000;
+  width: 70%;
+}
+.box{
+  border: 2px solid #18cd03;
+  width: 100%;
+  padding: 10px;
+}
+p{
+  color: #fff;
+}
+.box.bottom{
+  height: 300px;
+}
+.content-box{
+  padding: 20px;
+}
+.box.bottom.right-box img{
+  width: 100%;
+  opacity: 0.5;
+  margin-bottom: 10px;
+}
+.box.bottom.right-box{
+  border:none;
+}
+.box.bottom.right-box img:hover{
+  opacity: 1;
+}
+.context{
+  height: 400px;
+}
+.context img{
+  width: 100%;
+}
+.bottom-line{
+  height: 50px;
+  background:#232323;
+}
+#catalog-con{
+  display:none;
+}
+.left-box ul{
+  list-style: none;
+  color: #fff;
+  width: 100%;
+  margin: 0px;
+  padding: 0px;
+}
+.left-box ul li{
+  background:#232323;
+  padding: 5px;
+  margin: 1px;
+  border-radius: 2px;
+  cursor: pointer;
+  -webkit-box-shadow: inset 0px -3px 6px 2px rgba(0,0,0,0.75);
+-moz-box-shadow: inset 0px -3px 6px 2px rgba(0,0,0,0.75);
+box-shadow: inset 0px -3px 6px 2px rgba(0,0,0,0.75);
+}
+.left-box ul li a{
+  color: #fff;
+}
+.left-box ul li:hover{
+  background:#18cd03;
+}
+.left-box ul li a:hover{
+  text-decoration: none;
+  color: #fff;
+}
+.left-box ul li:hover .menu-box{
+  border:1px solid #fcd200;
+}
+#sideslide p{
+  font-size: 10px;
+}
+#sideslide img{
+  width: 100%;
+}
+.menu-box {
+  width: 8px;
+  height: 8px;
+  border: 1px solid #18cd03;
+  float: left;
+  margin:8px;
+}
+#home-box{
+  background:#18cd03;
+}
+.bottom-line ul{
+  list-style: none;
+}
+.bottom-line ul li{
+      float: right;
+    background: green;
+    padding: 2px 15px;
+    margin: 4px;
+    border-radius: 20px;
+    line-height: 35px;
+}
+
+</style>
+<body>
+  <div class="container">
+    <div class="container main-conatiner">
+      <div class="row">
+        <div class="content-box col-8 ">
+          <div class="tv">
+            <div class="screen-inner">
+              <div class="screen-content">
+                <div class="box right-box">
+                  <div class="context">
+                    <div class="row content-con" id="home-con">
+                      <div class="col-8">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer. typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.</p>
+                      </div>
+                      <div class="col-4">
+                        <img src="cam.jpg" alt="">
+                      </div>
+                      <div class="col-12">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer. typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.</p>
+                      </div>
+                    </div>
+                    <div class="row content-con" id="catalog-con">
+                      <div class="col-4">
+                        <img src="cam.jpg" alt="">
+                      </div>
+                      <div class="col-8">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer. typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.</p>
+                      </div>
+                      <div class="col-12">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer. typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="bottom-line">
+                    <ul>
+                      <li>1</li>
+                      <li>2</li>
+                      <li>3</li>
+                      <li>4</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="content-box col-4">
+          <div class="box left-box">
+            <ul class="menu-list" id="menu">
+              <li class="menu-list" id="home-list"><div class="menu-box" id="home-box"></div><a href="#" class="tv-btn" id="home">Home</a></li>
+              <li class="menu-list" id="catalog-list"><div class="menu-box" id="catalog-box"></div><a href="#" class="tv-btn" id="catalog">Catalog</a></li>
+              <li class="menu-list"><div class="menu-box"></div> Applications</li>
+              <li class="menu-list"><div class="menu-box"></div> Support and training</li>
+              <li class="menu-list"><div class="menu-box"></div> img Gallery</li>
+              <li class="menu-list"><div class="menu-box"></div> Video Gallery</li>
+              <li class="menu-list"><div class="menu-box"></div> Contact us</li>
+              <li class="menu-list"><div class="menu-box"></div> Product Order Page</li>
+            </ul>
+            <div class="row" id="sideslide">
+              <div class="col-6">
+                <img src="cam.jpg" alt="">
+              </div>
+              <div class="col-6"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio maiores neque atque porro rerum laborum sit tenetur eaque</p></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="content-box col-8">
+          <div class="box bottom right-box">
+            <img src="report2.gif" alt="">
+            <img src="skywatchad.jpg" alt="">
+          </div>
+        </div>
+        <div class="content-box col-4">
+          <div class="box bottom left-box"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+<script>
+var $tv = $('.tv')
+  ,$screen = $('.screen-content')
+  ,$btn = $('.tv-btn')
+  ,videoHtml = '<iframe src="//player.vimeo.com/video/25323516?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="590" height="330" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+
+$btn.on('click', function(){
+  var id=$(this).attr("id");
+  $(".menu-box").css("background","none");
+  $(".menu-list").css("background","#232323");
+  $(".content-con").css("display","none");
+  var idto="#"+id+"-con";
+  var idbto="#"+id+"-box";
+  var idlto="#"+id+"-list";
+  $(idto).css("display","flex");
+  $(idlto).css("background","#18cd03");
+  if ($tv.hasClass('_off')) {
+    $tv.removeClass('_off');
+  } else {
+    $tv.addClass('_off').delay(1000).queue(function(next){
+    $(this).removeClass('_off');
+    next();
+  });
+  }
+})
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</body>
+</html>
